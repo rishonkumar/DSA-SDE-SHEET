c0e53ab1c582159dbffcb6e2f49d4b53a671feba
rishonkumar <rishonkumarrahi@gmail.com>
2024-10-06 20:13:12 +0530

    Kadane Algo

---
+++ src/ArraysProblem/MaximumSubarray.java
@@ -0,0 +1,27 @@
+package ArraysProblem;
+
+public class MaximumSubarray {
+
+    /*
+    [-2,1,-3,4,-1,2,1,-5,4]
+
+    If any moment sum is -ve make sum = 0
+
+     */
+    public int maxSubArray(int[] nums) {
+
+        int n = nums.length;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+
+            if (sum > max) max = sum;
+
+            if (sum < 0) sum = 0;
+
+        }
+        return max;
+    }
+}
