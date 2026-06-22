2c92d49bbdc1c30b3a7e27d0192ed9dcdc52bd32
rishonkumar <rishon.kumar.rahi@gmail.com>
2026-06-22 08:30:06 +0530

    Create TwoSums.java

---
+++ src/Xlri8/Arrays/TwoSums.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int present = target - nums[i];
+            if (map.containsKey(present)) {
+                return new int[] { map.get(present), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
